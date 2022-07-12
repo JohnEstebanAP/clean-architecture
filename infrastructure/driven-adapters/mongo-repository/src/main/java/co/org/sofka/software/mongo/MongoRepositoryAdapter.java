281982f6b7c3021e081ebaa690fc3eda783e81f5
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-07-12 11:10:49 -0500

    reparación del bug en la configuración com MongoDb

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/org/sofka/software/mongo/MongoRepositoryAdapter.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/org/sofka/software/mongo/MongoRepositoryAdapter.java
@@ -8,16 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MongoRepositoryAdapter extends AdapterOperations<Pet, PetDocument, String, MongoDBRepository>
-implements PetRepository
-{
+public class MongoRepositoryAdapter
+    extends AdapterOperations<Pet, PetDocument, String, MongoDBRepository>
+    implements PetRepository {
 
-    public MongoRepositoryAdapter(MongoDBRepository repository, ObjectMapper mapper) {
-        /**
-         *  Could be use mapper.mapBuilder if your domain model implement builder pattern
-         *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
-         *  Or using mapper.map with the class of the object model
-         */
-        super(repository, mapper, d -> mapper.map(d, Pet.class));
-    }
+  public MongoRepositoryAdapter(MongoDBRepository repository, ObjectMapper mapper) {
+    /**
+     * Could be use mapper.mapBuilder if your domain model implement builder pattern
+     * super(repository, mapper, d ->
+     * mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build()); Or using mapper.map with
+     * the class of the object model
+     */
+    super(repository, mapper, d -> mapper.map(d, Pet.class));
+  }
 }
